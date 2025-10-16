b23823db06f7696928b200095a73f5b07f68b1a3
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-16 15:50:06 +0000

    distanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMilesdistanceInMiles

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+value = 1;
+total = 100;
 
+let distanceInMiles = function (value){
+    let factor = 1;
+    return value * total * factor;
+}
+
+document.getElementById("result").innerHTML = distanceInMiles;
